c769ecf0d586108f0e17564533a4f53b4496c752
Scottie <mr.horne@gmail.com>
2021-03-12 05:31:58 -0600

    corrected css working on index.html

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -3,20 +3,21 @@ This code centers the map on the closest bus and then for each bus, drops a
 pin at the bus' location.
 */
 
-var map;
+let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
         zoom: 15,
         scrollwheel: false
     });
+    let bus = "/bus.png";
 
     for (i=0; i<busLocations.length; i++){
         var marker = new google.maps.Marker({
             position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
             map: map,
             icon: {
-                    url: "/bus.png",
+                    url: bus,
                     scaledSize: new google.maps.Size(20, 20)
             }
         });
