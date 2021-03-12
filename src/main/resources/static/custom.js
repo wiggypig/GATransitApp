5a9e075c0bce49510c941f4ec963fdd930336183
Scottie <mr.horne@gmail.com>
2021-03-12 06:20:45 -0600

    corrected custom.js working on index.html

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -6,20 +6,36 @@ pin at the bus' location.
 let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
+        center: {
+                      lat: parseFloat(busLocations[0].LATITUDE),
+                      lng: parseFloat(busLocations[0].LONGITUDE),
+                },
         zoom: 15,
         scrollwheel: false
     });
     let bus = "/bus.png";
 
     for (i=0; i<busLocations.length; i++){
-        var marker = new google.maps.Marker({
-            position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
-            map: map,
-            icon: {
+            let marker = new google.maps.Marker({
+                position: {
+                    lat: parseFloat(busLocations[i].LATITUDE),
+                    lng: parseFloat(busLocations[i].LONGITUDE)
+                },
+                map: map,
+                icon: {
                     url: bus,
                     scaledSize: new google.maps.Size(20, 20)
-            }
-        });
-    }
+                },
+    //            animation: google.maps.Animation.BOUNCE,
+            });
+
+            let busNum = busLocations[i].VEHICLE;
+            let contentString = '<h4> Bus#:' + busNum + '</h4>';
+            let infowindow = new google.maps.InfoWindow({
+                content: contentString
+            })
+            google.maps.event.addListener(marker, 'click', () => infowindow.open(map, marker));
+
+        }//end for
 }
+
